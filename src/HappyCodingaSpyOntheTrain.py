588a70a249d54471eb4549f8429c2efa925874c5
niklas <oz3angam3r@gmail.com>
2023-04-25 23:07:16 +0200

    Änderung Prüfen anfung und auf leere

--- src/HappyCodingaSpyOntheTrain.py
+++ src/HappyCodingaSpyOntheTrain.py
@@ -2,16 +2,21 @@ def length_of_railway(sounds):
     #Banhof = Rein or Raus 呜呜呜
     #Auf_Schiene = 10 or 20 哐当
     Geräusche = list(sounds)
-    if sounds == []:
+    Anfang = list(sounds)
+    if sounds == [] or sounds == "":
         return[]
     print(Geräusche)
     
     Geschwindigkeit = 0
     Im_Bahnhof = True
     Bahnhoferstemal = 0
-    
+    Anfang = Anfang.pop(0)
+    if Anfang == "哐" :
+        Geschwindigkeit =0
+        return Geschwindigkeit
     for alles in range(len(Geräusche)):
         teil_von_Geräusche = Geräusche.pop(0)
+        
         if teil_von_Geräusche == "呜":
             Bahnhoferstemal = Bahnhoferstemal + 1
             
@@ -20,6 +25,7 @@ def length_of_railway(sounds):
                 Im_Bahnhof = not False
                 Bahnhoferstemal = 0
         elif teil_von_Geräusche == "哐":
+        
 
             if Im_Bahnhof == False:
                 Geschwindigkeit = Geschwindigkeit + 20
