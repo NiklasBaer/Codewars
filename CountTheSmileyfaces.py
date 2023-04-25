7757e5e6f33eeaaa5046908df0af7a036bff8950
niklas <oz3angam3r@gmail.com>
2023-04-25 15:58:36 +0200

    Test zu Count Smiley und Print rausnehmen

--- CountTheSmileyfaces.py
+++ CountTheSmileyfaces.py
@@ -1,15 +1,11 @@
 def count_smileys(arr):
     Count = 0
     for Smiley in arr:
-        print(Smiley)
         if len(Smiley) == 2:
             HatAugen = ":" in Smiley or ";" in Smiley
-            print(HatAugen)
             HatMund = ")" in Smiley or "D" in Smiley
-            print(HatMund)
             if HatMund == True and HatAugen == True:
                 Count = Count + 1
-        print(Count)
         if len(Smiley) == 3:
             HatAugen = ":" in Smiley or ";" in Smiley
             HatMund = ")" in Smiley or "D" in Smiley
