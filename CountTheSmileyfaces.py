9a8935b61f825af92f896701ca122cc6ab85e1c2
niklas <oz3angam3r@gmail.com>
2023-04-25 11:35:47 +0200

    Lösung für die Aufgabe

--- CountTheSmileyfaces.py
+++ CountTheSmileyfaces.py
@@ -1,16 +1,25 @@
-def Count_smileys(arr):
+def count_smileys(arr):
     Count = 0
     for Smiley in arr:
         print(Smiley)
-        HatAugen = ":" in Smiley or ";" in Smiley
-        print(HatAugen)
-        HatMund = ")" in Smiley or "(" in Smiley or "D" in Smiley
-        print(HatMund)
-        if HatMund == True and HatAugen == True:
-            Count = Count + 1
+        if len(Smiley) == 2:
+            HatAugen = ":" in Smiley or ";" in Smiley
+            print(HatAugen)
+            HatMund = ")" in Smiley or "D" in Smiley
+            print(HatMund)
+            if HatMund == True and HatAugen == True:
+                Count = Count + 1
         print(Count)
+        if len(Smiley) == 3:
+            HatAugen = ":" in Smiley or ";" in Smiley
+            HatMund = ")" in Smiley or "D" in Smiley
+            HatNase = "-" in Smiley or "~" in Smiley
+            if HatAugen == True and HatMund == True and HatNase == True:
+                Count = Count + 1
+        
     return Count
 
 
 
-Count_smileys([':D',':~)',';~D',';)'])
+count_smileys([':)',':(',':D',':O',':;'])
+count_smileys([";o)"])
