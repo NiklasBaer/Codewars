c256bb1666bee9811d9b08900036ba97668c2579
Adrian Schurz <adrian.schurz@check24.de>
2023-04-25 14:40:14 +0200

    CodeReview, Typo behoben, Formatierung, Fehlernachrichten für Tests

--- SimpleMultiplicationTest.py
+++ SimpleMultiplicationTest.py
@@ -2,16 +2,24 @@ import unittest
 
 from SimpleMultiplication import simple_multiplication
 
+
 class TestSimpleMultiplication(unittest.TestCase):
 
     def test_zero(self):
-        self.assertEqual(simple_multiplication(0), 0)
+        self.assertEqual(
+            simple_multiplication(0), 0,
+            "Multiplikation mit 0 muss immer 0 ergeben.")
 
     def test_even_number(self):
-        self.assertEqual(simple_multiplication(14), 112)
-    
-    def test_even_number(self):
-        self.assertEqual(simple_multiplication(3), 27)
+        self.assertEqual(
+            simple_multiplication(14), 112,
+            "Eine gerade Zahl sollte mit 8 multipliziert werden.")
+
+    def test_odd_number(self):
+        self.assertEqual(
+            simple_multiplication(3), 27,
+            "Eine ungerade Zahl sollte mit 9 multipliziert werden.")
+
 
 if __name__ == '__main__':
     unittest.main()
