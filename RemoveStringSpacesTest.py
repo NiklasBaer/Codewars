2dd455ba70112db06152c0bfb1ca110cca3f47eb
niklas <oz3angam3r@gmail.com>
2023-04-25 15:22:15 +0200

    Test zum Remove String

---
+++ RemoveStringSpacesTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+from RemoveStringSpaces import no_space
+
+class TestRemoveStringsSpaces(unittest.TestCase):
+    
+    def test_langerText(self):
+        self.assertEqual(no_space("Ich habe keine Ahnung was ich hier rein schreiben soll ich weiß nur das ich gerade irgend ein mist schreibe und es sich gerade irgendwer durchließt"), "IchhabekeineAhnungwasichhierreinschreibensollichweißnurdasichgeradeirgendeinmistschreibeundessichgeradeirgendwerdurchließt")
+        
+    def test_langeLeerzeichen(self):
+        self.assertEqual(no_space("Warum       mache       ich     das    hier   "), "Warummacheichdashier") 
+           
+if __name__ == "__main__" :
+    unittest.main()
