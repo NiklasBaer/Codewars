80092057543a43bdb40f799ca0e3ead11fd17d1a
niklas <oz3angam3r@gmail.com>
2023-04-25 11:06:48 +0200

    Prüfung auf einzelnes Augenpaar

---
+++ CountTheSmileyfaces.py
@@ -0,0 +1,12 @@
+def Count_smileys(arr):
+    Count = 0
+    for Smiley in arr:
+        print(Smiley)
+        HatAugen = ":" in Smiley or ";" in Smiley
+        print(HatAugen)
+        
+    return 
+
+
+
+Count_smileys([':D',':~)',';~D',';)'])
