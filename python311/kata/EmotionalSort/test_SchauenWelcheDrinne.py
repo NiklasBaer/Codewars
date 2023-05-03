c714109f004318e3112b151b32e2888a3ca46cde
niklas <oz3angam3r@gmail.com>
2023-05-03 09:42:17 +0200

    Änderungen

--- python311/kata/EmotionalSort/test_SchauenWelcheDrinne.py
+++ python311/kata/EmotionalSort/test_SchauenWelcheDrinne.py
@@ -1,40 +1,9 @@
 import unittest
 
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import SchauenWelcheDrinnen
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import SchauenInWelcheRichtung
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import Ergebnis
-@unittest.skip
+
 class TestSchauenWelcheDrinne(unittest.TestCase):
-    
-    def test_instanziirung(self):
-        x = SchauenWelcheDrinnen("Test")
-    
-    def test_wie_viele_drinnen(self):
-        x = SchauenWelcheDrinnen([":("])
-        y = x.Schauenwelchedrinne()
-        self.assertEqual(y, [":("])
-        
-    def test_schauen_welche_Richtung(self):
-        x = SchauenInWelcheRichtung(True)
-        y = x.SchauenWelcheRichtung()
-        self.assertEqual(y, 1)
-        x = SchauenInWelcheRichtung(False)
-        y = x.SchauenWelcheRichtung()
-        self.assertEqual(y, -1)
-        
-    def test_kommt_alles_an(self):
-        x = SchauenWelcheDrinnen([":D", ":D", ":D"])
-        y = x.Schauenwelchedrinne()
-        self.assertEqual(y,[":D",":D", ":D"])
-        
-    def test_wenn_unterschiedliche_Zeichen(self):
-        x = SchauenWelcheDrinnen([ ':D', 'T_T', ':D', ':(' ])
-        y = x.Schauenwelchedrinne()
-        self.assertEqual(y,  [':D', 'T_T', ':D', ':('])    
-        
-    def test_ob_ankommt(self):
-        x = Ergebnis([ ':D', 'T_T', ':D', ':(' ], True)
-        y = x.Lösung()
-        self.assertEqual(y,[])
-if __name__ == "__main__" : 
-    unittest.main() 
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
