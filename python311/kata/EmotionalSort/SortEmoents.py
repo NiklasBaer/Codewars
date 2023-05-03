1d3515baa1bcd1b05fd5f6c1c7ddcf2bdad313be
niklas <oz3angam3r@gmail.com>
2023-05-03 09:41:41 +0200

    Lösung zur Aufgabe

--- python311/kata/EmotionalSort/SortEmoents.py
+++ python311/kata/EmotionalSort/SortEmoents.py
@@ -1,22 +1,58 @@
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import SchauenWelcheDrinnen
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import SchauenInWelcheRichtung
-from python311.kata.EmotionalSort.SchauenWelcheDrinne import Ergebnis
-
 def sort_emotions(arr, order):
-    #Schauen welche zeichen drinne sind
-    #Schauen ob man es von gut nach schlecht machen soll oder von schlecht nach gut
-    #Liste weider zsm stellen und rausgeben
-    
-    Welche_drinne = SchauenWelcheDrinnen(arr)
-    WieVieleWelche = Welche_drinne.Schauenwelchedrinne()
-        
-    Welche_Richtung = SchauenInWelcheRichtung(order)
-    WelcheRichtung = Welche_Richtung.SchauenWelcheRichtung
-    
-    ZwischenErgebnis = Ergebnis(WieVieleWelche, Welche_Richtung)
-    Result = ZwischenErgebnis.Lösung()
-    
-    #Die KLssen sind so weit fertig muss nur noch an der Lösung gebastelt werden und ein paar bugfixes gemacht werden alles funktioniert so bis auf die einen Unittest 
-    
-sort_emotions([ ':D', 'T_T', ':D', ':(' ], True)
+    # Schauen welche zeichen drinne sind
+    # Schauen ob man es von gut nach schlecht machen soll oder von schlecht nach gut
+    # Liste weider zsm stellen und rausgeben
+    Simile = Codirer(arr, order)
+    SimileCodeer = Simile.Verarbeitung()
+    return SimileCodeer
 
+
+class Codirer():
+
+    def __init__(self, input, order) -> None:
+        self.SmilieLIste = input
+        self.Smilieorder = order
+
+    def DictonarySmylie(self, arr):
+
+        Smilie = {
+            ":D": 1,
+            ":)": 2,
+            ":|": 3,
+            ":(": 4,
+            "T_T": 5
+        }
+
+        return Smilie[arr]
+
+    def DictonarySmylieCode(self, Verarbeitet):
+
+        SmilieCodiert = {
+            1: ":D",
+            2: ":)",
+            3: ":|",
+            4: ":(",
+            5: "T_T"
+        }
+
+        return SmilieCodiert[Verarbeitet]
+
+    def Verarbeitung(self):
+        Zwichenliste = map(self.DictonarySmylie, self.SmilieLIste)
+        MapVerarbeiteteListe = list(Zwichenliste)
+        print(MapVerarbeiteteListe)
+        MapVerarbeiteteListe.sort()
+        print(MapVerarbeiteteListe)
+        if self.Smilieorder == True:
+            abfrageTrue = map(self.DictonarySmylieCode, MapVerarbeiteteListe)
+            ErgbenisTrue = list(abfrageTrue)
+            return ErgbenisTrue
+
+        if self.Smilieorder == False:
+            MapVerarbeiteteListe.sort(reverse=True)
+            abfrageFalse = map(self.DictonarySmylieCode, MapVerarbeiteteListe)
+            ErgbenisFalse = list(abfrageFalse)
+            return ErgbenisFalse
+
+
+sort_emotions([':D', 'T_T', ':D', ':('], True)
